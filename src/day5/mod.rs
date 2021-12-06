b1e6bf76f7f0c008f2963318d4ab4970cb689359
Rasmus Henriksson <rasmhenriksson@gmail.com>
2021-12-06 22:30:43 +0100

    Simplified read_input of Day 5

--- src/day5/mod.rs
+++ src/day5/mod.rs
@@ -2,10 +2,23 @@ use std::{fs, io::BufRead, io::BufReader};
 
 type Vec2 = [u32; 2];
 
-#[derive(Debug)]
+#[derive(Debug, Default, Clone)]
 struct Line {
     from: Vec2,
     to: Vec2,
+}
+
+impl Line {
+    fn new(to: Vec2, from: Vec2) -> Self {
+        Self { to, from }
+    }
+
+    fn from_raw(vals: &[u32]) -> Self {
+        Self {
+            from: [vals[0], vals[1]],
+            to: [vals[2], vals[3]],
+        }
+    }
 }
 
 fn read_input() -> Vec<Line> {
@@ -16,26 +29,26 @@ fn read_input() -> Vec<Line> {
     let mut parsed_input: Vec<Line> = Vec::new();
     for l in contents.lines() {
         if let Ok(result) = l {
-            let line_def = result.split(" -> ").collect::<Vec<&str>>();
-            if !line_def.is_empty() {
-                let from = line_def[0]
-                    .split(',')
-                    .filter_map(|s| s.parse::<u32>().ok())
-                    .collect::<Vec<u32>>();
-                let to = line_def[1]
-                    .split(',')
-                    .filter_map(|s| s.parse::<u32>().ok())
-                    .collect::<Vec<u32>>();
+            let values = result
+                .split(|c: char| !c.is_numeric())
+                .filter_map(|s| s.parse::<u32>().ok())
+                .collect::<Vec<u32>>();
 
-                parsed_input.push(Line {
-                    from: from.try_into().unwrap(),
-                    to: to.try_into().unwrap(),
-                });
+            if !values.is_empty() {
+                parsed_input.push(Line::from_raw(&values));
             }
         }
     }
-
+    assert_eq!(parsed_input.len(), 500);
     parsed_input
+}
+
+fn filter_to_axis_aligned(all_lines: &Vec<Line>) -> Vec<Line> {
+    all_lines
+        .iter()
+        .filter(|line| line.from[0] == line.to[0] || line.from[1] == line.to[1])
+        .cloned()
+        .collect::<Vec<Line>>()
 }
 
 pub fn run() {
@@ -44,4 +57,27 @@ pub fn run() {
 
 mod tests {
     use super::*;
+
+    fn get_input() -> Vec<Line> {
+        vec![
+            Line::from_raw(&[0, 9, 5, 9]),
+            Line::from_raw(&[8, 0, 0, 8]),
+            Line::from_raw(&[9, 4, 3, 4]),
+            Line::from_raw(&[2, 2, 2, 1]),
+            Line::from_raw(&[7, 0, 7, 4]),
+            Line::from_raw(&[6, 4, 2, 0]),
+            Line::from_raw(&[0, 9, 2, 9]),
+            Line::from_raw(&[3, 4, 1, 4]),
+            Line::from_raw(&[0, 0, 8, 8]),
+            Line::from_raw(&[5, 5, 8, 2]),
+        ]
+    }
+
+    #[test]
+    fn test_filter_to_axis_aligned() {
+        let inputs = get_input();
+        assert_eq!(inputs.len(), 10);
+        let filtered = filter_to_axis_aligned(&inputs);
+        assert_eq!(filtered.len(), 6);
+    }
 }
